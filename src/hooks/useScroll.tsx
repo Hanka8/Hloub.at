cdd90d62093aa87cf8e00804a519e7aa458cf19c
Hanka8 <hanka.maruskevicova@gmail.com>
2024-05-10 08:48:49 +0200

    throttle removed

--- src/hooks/useScroll.tsx
+++ src/hooks/useScroll.tsx
@@ -1,29 +1,20 @@
 import { useState, useEffect } from 'react';
 
-const useScroll = (throttleMs: number = 0): number => {
+const useScroll = (): number => {
     const [scrollY, setScrollY] = useState<number>(0);
 
     useEffect(() => {
-        let throttleTimeout: NodeJS.Timeout | null = null;
 
         const handleScroll = () => {
-            if (!throttleTimeout) {
-                throttleTimeout = setTimeout(() => {
-                    setScrollY(window.scrollY);
-                    throttleTimeout = null;
-                }, throttleMs);
-            }
+            setScrollY(window.scrollY);
         };
 
         window.addEventListener('scroll', handleScroll);
 
         return () => {
             window.removeEventListener('scroll', handleScroll);
-            if (throttleTimeout) {
-                clearTimeout(throttleTimeout);
-            }
         };
-    }, [throttleMs]);
+    }, [scrollY]);
 
     return scrollY;
 };
